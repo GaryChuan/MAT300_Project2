c9ba6138141cf6c2420843d5caec12faf2a22db1
Gary Chuan <garychuanzh@gmail.com>
2022-07-10 01:25:07 +0800

    Added TMP and point visual

--- Assets/Scripts/Visuals/DataVisual.cs
+++ Assets/Scripts/Visuals/DataVisual.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DataVisual : MonoBehaviour
 {
+    [Header("Derivatives")]
     [SerializeField] DerivativeVisual _velocityArrow;
     [SerializeField] DerivativeVisual _accelerationArrow;
+
+    [Header("Point")]
     [SerializeField] SpriteRenderer _pointRenderer;
-
-
-    private void Awake()
-    {
-    }
+    [SerializeField] TMP_Text _pointLabel;
 
     public void SetPointLabel(string label)
     {
@@ -20,28 +20,52 @@ public class DataVisual : MonoBehaviour
 
     public void SetVelocityArrowActive(bool active)
     {
-
+        _velocityArrow.gameObject.SetActive(active);
     }
 
     public void SetAccelerationArrowActive(bool active)
     {
-
+        _accelerationArrow.gameObject.SetActive(active);
     }
 
     public void SetPointActive(bool active)
     {
-
+        _pointRenderer.gameObject.SetActive(active);
+        _pointLabel.gameObject.SetActive(active);
     }
 
     public void UpdateVelocityArrow(Vector3 velocity)
     {
-
+        UpdateArrow(_velocityArrow, velocity);
     }
 
     public void UpdateAccelerationArrow(Vector3 acceleration)
     {
-
+        UpdateArrow(_accelerationArrow, acceleration);
     }
 
+    void UpdateArrow(DerivativeVisual arrow, Vector3 vec)
+    {
+        arrow.AdjustArrow(vec);
+    }
 
+    public bool ToggleVelocityArrow()
+    {
+        return _velocityArrow.ToggleArrow();
+    }
+
+    public bool ToggleAccelerationArrow()
+    {
+        return _accelerationArrow.ToggleArrow();
+    }
+
+    public void ShowVelocityArrow()
+    {
+        _velocityArrow.ShowArrow();
+    }
+
+    public void ShowAccelerationArrow()
+    {
+        _accelerationArrow.ShowArrow();
+    }
 }
